dd87f4df02084489abd6dbbdcb901bd8b3ebb72c
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2024-04-06 09:17:31 +0700

    Add files via upload

---
+++ Pr2.1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+using System.Collections.Generic; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        int[] nums = { 1, 2, 3, 4, 5};
+        bool flag = false;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    if (i != j)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            } 
+        }
+        Console.WriteLine(flag);
+    } 
+}
---
+++ Pr2.2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System; 
+using System.Collections.Generic; 
+ 
+class Program 
+{
+    static void Main()
+    {
+        Student dima = new Student();
+        dima.AcademicPerformance = new int[5] { 2, 3, 4, 5, 5 };
+        dima.groupNumber = 624;
+        Console.WriteLine("Ведите номер группы");
+        dima.groupNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите фамилию");
+        dima.Surname = Console.ReadLine();
+        Console.WriteLine("Введите дату рождения в формате ГГГГ-ММ-ДД");
+        dima.DateOfBirth = DateOnly.Parse(Console.ReadLine());
+        dima.PrintStudent();
+    }
+}
+
+class Student
+{
+    public string Surname;
+    public DateOnly DateOfBirth = new DateOnly();
+    public int groupNumber;
+    public int[] AcademicPerformance;
+
+    public void PrintStudent()
+    {
+        Console.WriteLine($"Фамилия: {Surname} \n Дата рождения: {DateOfBirth} \n Номер группы: {groupNumber} \n");
+        Console.WriteLine("Оценки");
+        foreach (int x in AcademicPerformance)
+        {
+            Console.Write($"{x} ");
+        }
+    }
+}
