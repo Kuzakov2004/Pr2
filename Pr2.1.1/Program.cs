48e39e46898c9440c86a6aad0e288be29cd6a28f
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2024-04-06 09:12:26 +0700

    uygguiu

---
+++ Pr2.1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program {
+        static void Main(string[] args)
+        {
+                string S, J;
+                int count = 0;
+                J = Console.ReadLine();
+                S = Console.ReadLine();
+
+
+                foreach (char diamond in J)
+                {
+                        foreach (char stone in S)
+                        {
+                                if (stone == diamond)
+                                {
+                                        count++;
+                                }
+                        }
+                }
+                Console.WriteLine(count);
+        }
+}
---
+++ Pr2.1.2/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System; 
+using System.Collections.Generic; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        int[] candidates = { 2, 5, 2, 1, 2}; 
+        int target = 5; 
+        Array.Sort(candidates); 
+        var result = new List<List<int>>(); 
+        var list = new List<(int, int, List<int>)>(); 
+        list.Add((0, target, new List<int>())); 
+         
+        int index = 0; 
+        while (index < list.Count) 
+        { 
+            var (start, remainingTarget, current) = list[index]; 
+ 
+            if (remainingTarget == 0) 
+            { 
+                result.Add(new List<int>(current)); 
+            } 
+ 
+            for (int i = start; i < candidates.Length && candidates[i] <= remainingTarget; i++) 
+            { 
+                if (i > start && candidates[i] == candidates[i - 1]) 
+                { 
+                    continue; 
+                } 
+ 
+                var newCurrent = new List<int>(current); 
+                newCurrent.Add(candidates[i]); 
+                list.Add((i + 1, remainingTarget - candidates[i], newCurrent)); 
+            } 
+ 
+            index++; 
+        } 
+ 
+        foreach (var combination in result) 
+        { 
+            Console.WriteLine(string.Join(", ", combination)); 
+        } 
+    } 
+    
+}
