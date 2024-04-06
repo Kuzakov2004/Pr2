eb037a3c678582891e6d743783195c551e8c0b03
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2024-04-06 09:19:26 +0700

    Add files via upload

---
+++ Pr2.2.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System; 
+using System.Collections.Generic; 
+ 
+class Program 
+{
+    static void Main()
+    {
+        Train lastochka = new Train();
+        Train vostok = new Train();
+        Train zapad = new Train();
+
+        lastochka.TrainNumber = 624;
+        lastochka.NameOfDestination = "Бийск";
+        lastochka.DepartureTime = new TimeOnly(18, 30, 00);
+
+        vostok.TrainNumber = 931;
+        vostok.NameOfDestination = "Адлер";
+        vostok.DepartureTime = new TimeOnly(9, 00, 00);
+
+        zapad.TrainNumber = 567;
+        zapad.NameOfDestination = "Москва";
+        zapad.DepartureTime = new TimeOnly(15, 20, 00);
+        
+        
+        Console.WriteLine("Введите номер поезда (624, 931, 567) ");
+        int number = int.Parse(Console.ReadLine());
+
+        switch (number)
+        {
+            case 624:
+                lastochka.PrintTrain();
+                break;
+            case 931:
+                vostok.PrintTrain();
+                break;
+            case 567:
+                zapad.PrintTrain();
+                break;
+            default:
+                Console.WriteLine("Неверно набран номер поезда");
+                break;
+        }
+    }
+}
+
+class Train
+{
+    public string NameOfDestination;
+    public TimeOnly DepartureTime = new TimeOnly();
+    public int TrainNumber;
+
+    public void PrintTrain()
+    {
+        Console.WriteLine($"название пункта назначения: {NameOfDestination} \nномер поезда: {TrainNumber} \nвремя отправления: {DepartureTime} \n");
+    }
+}
---
+++ Pr2.2.4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System; 
+using System.Collections.Generic; 
+ 
+class Program 
+{
+    static void Main()
+    {
+        Counter example1 = new Counter();
+        Counter example2 = new Counter();
+
+        example2.Count = 10;
+
+        for (int i = 0; i < 5; i++)
+        {
+            example1.increase();
+            example2.decrease();
+        }
+        
+        Console.WriteLine(example1.Count);
+        Console.WriteLine(example2.Count);
+    }
+}
+
+class Counter
+{
+    private int count = 0;
+
+    public int Count
+    {
+        get { return count; }
+        set { count = value; }
+    }
+
+    public void increase()
+    {
+        Count++;
+    }
+    
+    public void decrease()
+    {
+        Count--;
+    }
+}
